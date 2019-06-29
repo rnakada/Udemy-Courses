a4d0ea6bae41a5d6192b68bfd9fa086ece23c0f1
Ryuichi Nakada <rnakada90@gmail.com>
2019-06-28 23:03:13 -0400

    finished Data-Structures course

--- DataStructures-in-Javascript/3.binarySearchTree.js
+++ DataStructures-in-Javascript/3.binarySearchTree.js
@@ -35,6 +35,28 @@ BST.prototype.depthFirstTraversal = function (iteratorFunc, order) {
     if (order === "post-order") iteratorFunc(this.value);
 }
 
+BST.prototype.breadthFirstTraversal = function (iteratorFunc) {
+    var queue = [this];
+    while (queue.length) {
+        var treeNode = queue.shift();
+        iteratorFunc(treeNode);
+        if (treeNode.left) queue.push(treeNode.left);
+        if (treeNode.right) queue.push(treeNode.right);
+    }
+}
+
+BST.prototype.getMinVal = function () {
+    if (this.left) return this.left.getMinVal();
+    else return this.value;
+};
+
+BST.prototype.getMaxVal = function () {
+    if (this.right) return this.right.getMaxVal();
+    else return this.value;
+};
+
+
+
 var bst = new BST(50);
 
 bst.insert(30);
@@ -49,12 +71,20 @@ bst.insert(85);
 bst.insert(105);
 bst.insert(10);
 
-bst.depthFirstTraversal(log, "post-order");
+// bst.depthFirstTraversal(log, "post-order");
 
-function log(value) {
-    console.log(value);
+function log(node) {
+    console.log(node.value);
 }
+
+bst.breadthFirstTraversal(log);
+
+// console.log(bst.breadthFirstTraversal(log));
 
 console.log(bst.contains(15));
 
 console.log(bst.right.right);
+
+
+console.log("MIN: ", bst.getMinVal());
+console.log("MAX: ", bst.getMaxVal());
---
+++ DataStructures-in-Javascript/charCodeAt-modulusOperator.js
@@ -0,0 +1,10 @@
+// charCodeAt()
+
+console.log("hello world".charCodeAt(4));
+// unicode of 111
+
+
+// Modulus Operator
+
+console.log(100 % 30);
+// remainder of 10
